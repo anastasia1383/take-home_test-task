414aa46dd3bc90603cda4a021636f117a932b49b
Anastasiia Vasylieva <anastasia.vasylieva97@gmail.com>
2024-06-07 13:27:50 +0200

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "./src/index.scss";`,
+export default defineConfig(({ mode }) => {
+  const base = mode === 'production' ? '/take-home_test-task/' : '/';
+
+  return {
+    plugins: [react()],
+    base,
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "./src/index.scss";`,
+        },
       },
     },
-  },
+  };
 });
