d5bc6d70ff94529f433ab0c8eaa31c2181d162e7
Anastasiia Vasylieva <anastasia.vasylieva97@gmail.com>
2024-06-07 13:01:51 +0200

    quest item and list added

---
+++ src/components/QuestsList/QuestsList.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { QuestItem } from '../QuestItem/QuestItem';
+import { Quest } from '../../types/types';
+import './QuestsList.scss';
+
+interface QuestsListProps {
+  quests: Quest[];
+}
+
+export const QuestsList: React.FC<QuestsListProps> = ({ quests }) => {
+  return (
+    <div className='quests-list'>
+      {quests.length > 0 ? (
+        <ul className='quests-list__container'>
+          {quests.map((quest) => (
+            <QuestItem key={quest.id} quest={quest} />
+          ))}
+        </ul>
+      ) : (
+        <p>No quests available</p>
+      )}
+    </div>
+  );
+};
