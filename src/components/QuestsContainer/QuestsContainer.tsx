cac65e02f26925331aa693b33b160f25d4027a82
Anastasiia Vasylieva <anastasia.vasylieva97@gmail.com>
2024-06-07 13:02:14 +0200

    quests container and tooltip added

---
+++ src/components/QuestsContainer/QuestsContainer.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { fetchData } from '../../helpers/fetchData';
+import { Quest } from '../../types/types';
+import { QuestsList } from '../QuestsList/QuestsList';
+import './QuestsContainer.scss';
+import { QuestsListTooltip } from '../QuestsListTooltip/QuestsListTooltip';
+
+export const QuestsContainer: React.FC = () => {
+  const [quests, setQuests] = useState<Quest[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const data = await fetchData();
+        setQuests(data);
+      } catch (error: unknown) {
+        const e = error as Error;
+        setError(e.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getData();
+  }, []);
+
+  if (loading) {
+    return <p>Loading quests...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  const tooltip = 'This is a quest list. This is a quest list. This is a quest list.';
+
+  return (
+    <div className="quests-container">
+      <div className="quests-container__header">
+        <h1 className="quests-container__title">Quest List</h1>
+        <QuestsListTooltip text={tooltip}/>
+      </div>
+
+      <QuestsList quests={quests}/>
+    </div>
+  );
+};
---
+++ src/components/QuestsListTooltip/QuestsListTooltip.tsx
@@ -0,0 +1,19 @@
+import tooltip from '../../icons/tooltip.svg';
+import './QuestsListTooltip.scss';
+
+interface QuestsListTooltipProps {
+  text: string;
+}
+
+export const QuestsListTooltip: React.FC<QuestsListTooltipProps> = ({
+  text,
+}) => {
+  return (
+    <div className='quests-list-tooltip'>
+      <div className="quests-list-tooltip__icon">
+        <img src={tooltip} alt="tooltip" />
+      </div>
+      <div className="quests-list-tooltip__text">{text}</div>
+    </div>
+  );
+};
