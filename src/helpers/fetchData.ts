ea8d6ae9ac30868033109a4c6523f29381a98dce
Anastasiia Vasylieva <anastasia.vasylieva97@gmail.com>
2024-06-07 13:50:28 +0200

    fixed public url import

--- src/helpers/fetchData.ts
+++ src/helpers/fetchData.ts
@@ -1,7 +1,7 @@
 import { Quest } from "../types/types";
 
 export const fetchData = async (): Promise<Quest[]> => {
-  const response = await fetch('../../public/quests.json');
+  const response = await fetch('./quests.json');
 
   if (!response.ok) {
     throw new Error('Network response was not ok');
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const base = mode === 'production' ? '/take-home_test-task/' : '/';
-
+export default defineConfig(() => {
   return {
     plugins: [react()],
-    base,
+    base: '/take-home_test-task/',
     css: {
       preprocessorOptions: {
         scss: {
